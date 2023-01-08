074213e417e2d3907ff31f95165ee3cf31533a75
Mehedi Hasan <72302078+MehediHasanRonnie@users.noreply.github.com>
2023-01-08 18:03:42 +0600

    commited

---
+++ firstPrograme/index.ts
@@ -0,0 +1,10 @@
+// let userNmae: string = "Mehedi hasan";
+// let roll = 1701010048;
+// console.log(userNmae);
+// console.log(roll);
+
+function addNumbers(num1: number, num2: number) {
+  console.log(num1 + num2);
+}
+addNumbers(20, 30);
+addNumbers(20, 40);
