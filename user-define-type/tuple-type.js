1705d626172d0b037235b2c4fa95736093ecdc5c
Mehedi Hasan <72302078+MehediHasanRonnie@users.noreply.github.com>
2023-01-09 18:34:53 +0600

    commited

---
+++ user-define-type/tuple-type.js
@@ -0,0 +1,8 @@
+//tuple data type which is use mixed data type for using key and value pair
+//defining tuple
+var user;
+user = [101, "Mehedi"];
+console.log(user);
+user.push(102, "hasan");
+console.log(user);
+console.log(user[1]);
---
+++ user-define-type/tuple-type.ts
@@ -0,0 +1,11 @@
+//tuple data type which is use mixed data type for using key and value pair
+
+//defining tuple
+
+let user: [number, string];
+user = [101, "Mehedi"];
+console.log(user);
+
+user.push(102, "hasan");
+console.log(user);
+console.log(user[1]);
